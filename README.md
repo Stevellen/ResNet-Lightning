8d7f7009e8dfe3920beedadfb888e18eb1908916
Stevellen <belcher.steven@gmail.com>
2021-05-31 11:51:21 -0500

    Adding Options and Updated Logging
    Added a fine-tune specific option to allow tuning of the entire resnet backbone. Updated logging format to use self.log rather than returning dictionaries of values. Some other small fixes

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 This is a quick Pytorch Lighning wrapper around the ResNet models provided by Torchvision. This model is reliant on torch, torchvision and pytorch-lightning packages, which must be installed separately. The model can be trained directly from the command line using the following arguments:
 
 * model - The ResNet variant to be used, e.g. 18, 50, etc.
-* num_classes - The number of target classes being trained
+* num_classes - The number of target classes being trained. In the case of a binary classification task, the model uses BCEWithLogitLoss, whereas with multiclass problems it uses CrossEntropyLoss.
 * num_epochs - The number of epochs for training
 * train_set - The path to the ***folder*** containing the training images. This folder should be divided into subfolders by class, with the label for each class; e.g. "train/cat/" and "train/dog/" for a classifier trained to recognize cats or dogs
 * vld_set - The path to the ***folder*** containing the validation images. This should be divided in the same way as the training folder
@@ -11,6 +11,7 @@ This is a quick Pytorch Lighning wrapper around the ResNet models provided by To
 * learning_rate [-lr] (optional) - Learning rate used in optimization; defaults to 1e-3
 * batch_size [-b] (optional) - Batch size used in training; defaults to 16
 * transfer [-tr] (optional) - Flag to indicate whether this is a transfer learning task or not; defaults to false, meaning the entire model will be trained unless this flag is provided
+* tune_fc_only [-to] (optional) - Indicates whether the model should only train the fully-connected output layers or fine-tune the resnet backbone as well; defaults to True
 * save_path [-s] (optional) - Path to directory where final model checkpoint will be saved; defaults to CWD.
 * gpus [-g] (optional) - Number of GPUs to use during training (note that this relies on having the CUDA version of PyTorch installed); defaults to 0, meaning CPU will be used
 
--- resnet_classifier.py
+++ resnet_classifier.py
@@ -13,16 +13,17 @@ from torch.optim import SGD, Adam
 from torchvision import transforms
 from torchvision.datasets import ImageFolder
 from torch.utils.data import DataLoader
-import pytorch_lightning as ptl
+import pytorch_lightning as pl
 
 # Here we define a new class to turn the ResNet model that we want to use as a feature extractor
 # into a pytorch-lightning module so that we can take advantage of lightning's Trainer object.
 # We aim to make it a little more general by allowing users to define the number of prediction classes.
-class ResNetClassifier(ptl.LightningModule):
+class ResNetClassifier(pl.LightningModule):
     def __init__(self, num_classes, resnet_version,
                 train_path, vld_path, test_path=None, 
-                optimizer='adam', lr=1e-3, batch_size=16, transfer=True):
-        super(ResNetClassifier, self).__init__()
+                optimizer='adam', lr=1e-3, batch_size=16,
+                transfer=True, tune_fc_only=True):
+        super().__init__()
 
         self.__dict__.update(locals())
         resnets = {
@@ -32,26 +33,28 @@ class ResNetClassifier(ptl.LightningModule):
         }
         optimizers = {'adam': Adam, 'sgd': SGD}
         self.optimizer = optimizers[optimizer]
+        #instantiate loss criterion
+        self.criterion = nn.BCEWithLogitsLoss() if num_classes == 2 else nn.CrossEntropyLoss()
         # Using a pretrained ResNet backbone
-        self.resnet_model = resnets[resnet_version](pretrained=True)
+        self.resnet_model = resnets[resnet_version](pretrained=transfer)
         # Replace old FC layer with Identity so we can train our own
         linear_size = list(self.resnet_model.children())[-1].in_features
         # replace final layer for fine tuning
         self.resnet_model.fc = nn.Linear(linear_size, num_classes)
 
-        if transfer:
+        if tune_fc_only: # option to only tune the fully-connected layers
             for child in list(self.resnet_model.children())[:-1]:
                 for param in child.parameters():
                     param.requires_grad = False
 
     def forward(self, X):
-        X = self.resnet_model(X)
-        return F.softmax(X, dim=1)
+        return self.resnet_model(X)
 
     def configure_optimizers(self):
-        return Adam(self.parameters(), lr=self.lr)
+        return self.optimizer(self.parameters(), lr=self.lr)
     
     def train_dataloader(self):
+        # values here are specific to pneumonia dataset and should be changed for custom data
         transform = transforms.Compose([
                 transforms.Resize((500,500)),
                 transforms.RandomHorizontalFlip(0.3),
@@ -62,77 +65,73 @@ class ResNetClassifier(ptl.LightningModule):
                 transforms.ToTensor(),
                 transforms.Normalize((0.48232,), (0.23051,))
         ])
-        pneumonia_train = ImageFolder(self.train_path, transform=transform)
-        return DataLoader(pneumonia_train, batch_size=self.batch_size, shuffle=True)
+        img_train = ImageFolder(self.train_path, transform=transform)
+        return DataLoader(img_train, batch_size=self.batch_size, shuffle=True)
     
     def training_step(self, batch, batch_idx):
         x, y = batch
         preds = self(x)
-        y = F.one_hot(y, num_classes=2).type(torch.FloatTensor)
+        if self.num_classes == 2:
+            y = F.one_hot(y, num_classes=2).float()
         
-        loss = F.binary_cross_entropy(preds, y)
+        loss = self.criterion(preds, y)
         acc = (torch.argmax(y,1) == torch.argmax(preds,1)) \
-                .type(torch.FloatTensor)
-        acc = acc.mean()
-        return {'loss': loss, 'acc': acc}
+                .type(torch.FloatTensor).mean()
+        # perform logging
+        self.log("train_loss", loss, on_step=True, on_epoch=True, prog_bar=True, logger=True)
+        self.log("train_acc", acc, on_step=True, on_epoch=True, prog_bar=True, logger=True)
+        return loss
 
     def val_dataloader(self):
+        # values here are specific to pneumonia dataset and should be changed for custom data
         transform = transforms.Compose([
                 transforms.Resize((500,500)),
                 transforms.ToTensor(),
                 transforms.Normalize((0.48232,), (0.23051,))
         ])
         
-        pneumonia_vld = ImageFolder(self.vld_path, transform=transform)
+        img_val = ImageFolder(self.vld_path, transform=transform)
         
-        return DataLoader(pneumonia_vld, batch_size=1, shuffle=False)
+        return DataLoader(img_val, batch_size=1, shuffle=False)
     
     def validation_step(self, batch, batch_idx):
         x, y = batch
         preds = self(x)
-        y = F.one_hot(y, num_classes=2).type(torch.FloatTensor)
+        if self.num_classes == 2:
+            y = F.one_hot(y, num_classes=2).float()
         
-        loss = F.binary_cross_entropy(preds, y)
+        loss = self.criterion(preds, y)
         acc = (torch.argmax(y,1) == torch.argmax(preds,1)) \
-                .type(torch.FloatTensor)
-        acc = acc.mean()
-        return {'val_loss': loss, 'val_acc': acc}
+                .type(torch.FloatTensor).mean()
+        # perform logging
+        self.log("val_loss", loss, on_epoch=True, prog_bar=True, logger=True)
+        self.log("val_acc", acc, on_epoch=True, prog_bar=True, logger=True)
 
-    def validation_epoch_end(self, outputs):
-        avg_loss = torch.stack([x['val_loss'] for x in outputs]).mean()
-        avg_acc = torch.stack([x['val_acc'] for x in outputs]).mean()
-
-        tensorboard_logs = {'val_loss': avg_loss, 'val_acc': avg_acc}
-        return {'val_loss': avg_loss, 'val_acc': avg_acc, 'log': tensorboard_logs}
 
     def test_dataloader(self):
+        # values here are specific to pneumonia dataset and should be changed for custom data
         transform = transforms.Compose([
                 transforms.Resize((500,500)),
                 transforms.ToTensor(),
                 transforms.Normalize((0.48232,), (0.23051,))
         ])
         
-        pneumonia_vld = ImageFolder(self.test_path, transform=transform)
+        img_test = ImageFolder(self.test_path, transform=transform)
         
-        return DataLoader(pneumonia_vld, batch_size=1, shuffle=False)
+        return DataLoader(img_test, batch_size=1, shuffle=False)
     
     def test_step(self, batch, batch_idx):
         x, y = batch
         preds = self(x)
-        y = F.one_hot(y, num_classes=2).type(torch.FloatTensor)
+        if self.num_classes == 2:
+            y = F.one_hot(y, num_classes=2).float()
         
-        loss = F.binary_cross_entropy(preds, y)
-        acc = (torch.argmax(y,1) == torch.argmax(preds, 1)) \
-                .type(torch.FloatTensor)
-        acc = acc.mean()
-        return {'test_loss': loss, 'test_acc': acc}
-
-    def test_epoch_end(self, outputs):
-        avg_loss = torch.stack([x['test_loss'] for x in outputs]).mean()
-        avg_acc = torch.stack([x['test_acc'] for x in outputs]).mean()
-
-        tensorboard_logs = {'test_loss': avg_loss, 'test_acc': avg_acc}
-        return {'test_loss': avg_loss, 'test_acc': avg_acc, 'log': tensorboard_logs}
+        loss = self.criterion(preds, y)
+        acc = (torch.argmax(y,1) == torch.argmax(preds,1)) \
+                .type(torch.FloatTensor).mean()
+        # perform logging
+        self.log("test_loss", loss, on_step=True, prog_bar=True, logger=True)
+        self.log("test_acc", acc, on_step=True, prog_bar=True, logger=True)
 
 
 if __name__ == "__main__":
@@ -154,17 +153,19 @@ if __name__ == "__main__":
     parser.add_argument("-tr", "--transfer",
                         help="""Determine whether to use pretrained model or train from scratch. Defaults to True.""",
                         action="store_true")
+    parser.add_argument("-to", "--tune_fc_only", help="Tune only the final, fully connected layers.", action="store_true")
     parser.add_argument("-s", "--save_path", help="""Path to save model trained model checkpoint.""")
-    parser.add_argument("-g", "--gpus", help="""Enables GPU acceleration.""", type=int, default=0)
+    parser.add_argument("-g", "--gpus", help="""Enables GPU acceleration.""", type=int, default=None)
     args = parser.parse_args()
 
     # # Instantiate Model
-    model = ResNetClassifier(args.num_classes, args.model, args.train_set,
-                            args.vld_set, args.test_set, args.optimizer,
-                            args.learning_rate, args.batch_size, args.transfer)
+    model = ResNetClassifier(num_classes = args.num_classes, resnet_version = args.model,
+                            train_path = args.train_set,vld_path = args.vld_set, test_path = args.test_set,
+                            optimizer = args.optimizer, lr = args.learning_rate,
+                            batch_size = args.batch_size, transfer = args.transfer, tune_fc_only = args.tune_fc_only)
     # Instantiate lightning trainer and train model
-    trainer_args = {'gpus': args.gpus, 'max_epochs': args.num_epochs} if args.gpus else {'max_epochs': args.num_epochs}
-    trainer = ptl.Trainer(**trainer_args)
+    trainer_args = {'gpus': args.gpus, 'max_epochs': args.num_epochs}
+    trainer = pl.Trainer(**trainer_args)
     trainer.fit(model)
     # Save trained model
     save_path = (args.save_path if args.save_path is not None else '/') + 'trained_model.ckpt'
