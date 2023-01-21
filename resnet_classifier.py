4d20adda74ffcd290c0d7860038a1cffdc4dbcc4
Stevellen <belcher.steven@gmail.com>
2023-01-21 17:16:05 -0600

    refactor & cleanup

--- resnet_classifier.py
+++ resnet_classifier.py
@@ -13,30 +13,44 @@ from torch.optim import SGD, Adam
 from torchvision import transforms
 from torchvision.datasets import ImageFolder
 from torch.utils.data import DataLoader
+from torchmetrics import Accuracy
 import pytorch_lightning as pl
 
 # Here we define a new class to turn the ResNet model that we want to use as a feature extractor
 # into a pytorch-lightning module so that we can take advantage of lightning's Trainer object.
 # We aim to make it a little more general by allowing users to define the number of prediction classes.
 class ResNetClassifier(pl.LightningModule):
-    def __init__(self, num_classes, resnet_version,
-                train_path, vld_path, test_path=None, 
-                optimizer='adam', lr=1e-3, batch_size=16,
-                transfer=True, tune_fc_only=True):
-        super().__init__()
-
-        self.__dict__.update(locals())
-        resnets = {
+    resnets = {
             18: models.resnet18, 34: models.resnet34,
             50: models.resnet50, 101: models.resnet101,
             152: models.resnet152
         }
-        optimizers = {'adam': Adam, 'sgd': SGD}
-        self.optimizer = optimizers[optimizer]
-        #instantiate loss criterion
-        self.criterion = nn.BCEWithLogitsLoss() if num_classes == 2 else nn.CrossEntropyLoss()
+    optimizers = {'adam': Adam, 'sgd': SGD}
+
+    def __init__(self, num_classes, resnet_version,
+                train_path, val_path, test_path=None, 
+                optimizer='adam', lr=1e-3, batch_size=16,
+                transfer=True, tune_fc_only=True):
+        super().__init__()
+
+        self.num_classes = num_classes
+        self.train_path = train_path
+        self.val_path = val_path
+        self.test_path = test_path
+        self.lr = lr
+        self.batch_size = batch_size
+
+        self.optimizer = self.optimizers[optimizer]
+        # instantiate loss criterion
+        self.loss_fn = nn.BCEWithLogitsLoss() if num_classes == 1 else nn.CrossEntropyLoss()
+        # create accuracy metric
+        self.acc = Accuracy(
+            task='binary' if num_classes == 1 else 'multiclass',
+            num_classes=num_classes
+
+        )
         # Using a pretrained ResNet backbone
-        self.resnet_model = resnets[resnet_version](pretrained=transfer)
+        self.resnet_model = self.resnets[resnet_version](pretrained=transfer)
         # Replace old FC layer with Identity so we can train our own
         linear_size = list(self.resnet_model.children())[-1].in_features
         # replace final layer for fine tuning
@@ -52,83 +66,57 @@ class ResNetClassifier(pl.LightningModule):
 
     def configure_optimizers(self):
         return self.optimizer(self.parameters(), lr=self.lr)
-    
-    def train_dataloader(self):
-        # values here are specific to pneumonia dataset and should be changed for custom data
+
+    def _step(self, batch):
+        x, y = batch
+        preds = self(x)
+
+        if self.num_classes == 1:
+            preds = preds.flatten()
+            y = y.float()
+
+        loss = self.loss_fn(preds, y)
+        acc = self.acc(preds, y)
+        return loss, acc
+
+    def _dataloader(self, data_path, shuffle=False):
+        # values here are specific to pneumonia dataset and should be updated for custom data
         transform = transforms.Compose([
-                transforms.Resize((500,500)),
-                transforms.RandomHorizontalFlip(0.3),
-                transforms.RandomVerticalFlip(0.3),
-                transforms.RandomApply([
-                    transforms.RandomRotation(180)                    
-                ]),
+                transforms.Resize((500, 500)),
                 transforms.ToTensor(),
                 transforms.Normalize((0.48232,), (0.23051,))
         ])
-        img_train = ImageFolder(self.train_path, transform=transform)
-        return DataLoader(img_train, batch_size=self.batch_size, shuffle=True)
-    
-    def training_step(self, batch, batch_idx):
-        x, y = batch
-        preds = self(x)
-        if self.num_classes == 2:
-            y = F.one_hot(y, num_classes=2).float()
         
-        loss = self.criterion(preds, y)
-        acc = (torch.argmax(y,1) == torch.argmax(preds,1)) \
-                .type(torch.FloatTensor).mean()
+        img_folder = ImageFolder(data_path, transform=transform)
+        
+        return DataLoader(img_folder, batch_size=self.batch_size, shuffle=shuffle)
+
+    
+    def train_dataloader(self):
+        return self._dataloader(self.train_path, shuffle=True)
+
+    def training_step(self, batch, batch_idx):
+        loss, acc = self._step(batch)
         # perform logging
         self.log("train_loss", loss, on_step=True, on_epoch=True, prog_bar=True, logger=True)
         self.log("train_acc", acc, on_step=True, on_epoch=True, prog_bar=True, logger=True)
         return loss
 
     def val_dataloader(self):
-        # values here are specific to pneumonia dataset and should be changed for custom data
-        transform = transforms.Compose([
-                transforms.Resize((500,500)),
-                transforms.ToTensor(),
-                transforms.Normalize((0.48232,), (0.23051,))
-        ])
-        
-        img_val = ImageFolder(self.vld_path, transform=transform)
-        
-        return DataLoader(img_val, batch_size=1, shuffle=False)
+        return self._dataloader(self.val_path)
     
     def validation_step(self, batch, batch_idx):
-        x, y = batch
-        preds = self(x)
-        if self.num_classes == 2:
-            y = F.one_hot(y, num_classes=2).float()
-        
-        loss = self.criterion(preds, y)
-        acc = (torch.argmax(y,1) == torch.argmax(preds,1)) \
-                .type(torch.FloatTensor).mean()
+        loss, acc = self._step(batch)
         # perform logging
         self.log("val_loss", loss, on_epoch=True, prog_bar=False, logger=True)
         self.log("val_acc", acc, on_epoch=True, prog_bar=True, logger=True)
 
 
     def test_dataloader(self):
-        # values here are specific to pneumonia dataset and should be changed for custom data
-        transform = transforms.Compose([
-                transforms.Resize((500,500)),
-                transforms.ToTensor(),
-                transforms.Normalize((0.48232,), (0.23051,))
-        ])
-        
-        img_test = ImageFolder(self.test_path, transform=transform)
-        
-        return DataLoader(img_test, batch_size=1, shuffle=False)
+        return self._dataloader(self.test_path)
     
     def test_step(self, batch, batch_idx):
-        x, y = batch
-        preds = self(x)
-        if self.num_classes == 2:
-            y = F.one_hot(y, num_classes=2).float()
-        
-        loss = self.criterion(preds, y)
-        acc = (torch.argmax(y,1) == torch.argmax(preds,1)) \
-                .type(torch.FloatTensor).mean()
+        loss, acc = self._step(batch)
         # perform logging
         self.log("test_loss", loss, on_step=True, prog_bar=True, logger=True)
         self.log("test_acc", acc, on_step=True, prog_bar=True, logger=True)
@@ -143,7 +131,7 @@ if __name__ == "__main__":
     parser.add_argument("num_classes", help="""Number of classes to be learned.""", type=int)
     parser.add_argument("num_epochs", help="""Number of Epochs to Run.""", type=int)
     parser.add_argument("train_set", help="""Path to training data folder.""", type=Path)
-    parser.add_argument("vld_set", help="""Path to validation set folder.""", type=Path)
+    parser.add_argument("val_set", help="""Path to validation set folder.""", type=Path)
     # Optional arguments
     parser.add_argument('-amp', '--mixed_precision', help="""Use mixed precision during training. Defaults to False.""", action="store_true")
     parser.add_argument("-ts", "--test_set", help="""Optional test set path.""", type=Path)
@@ -161,12 +149,13 @@ if __name__ == "__main__":
 
     # # Instantiate Model
     model = ResNetClassifier(num_classes = args.num_classes, resnet_version = args.model,
-                            train_path = args.train_set,vld_path = args.vld_set, test_path = args.test_set,
+                            train_path = args.train_set, val_path = args.val_set, test_path = args.test_set,
                             optimizer = args.optimizer, lr = args.learning_rate,
                             batch_size = args.batch_size, transfer = args.transfer, tune_fc_only = args.tune_fc_only)
 
+    save_path = (args.save_path if args.save_path is not None else './models')
     checkpoint_callback = pl.callbacks.ModelCheckpoint(
-        dirpath=args.save_path,
+        dirpath=save_path,
         filename="resnet-model-{epoch}-{val_loss:.2f}-{val_acc:0.2f}",
         monitor='val_loss',
         save_top_k=3,
@@ -187,7 +176,5 @@ if __name__ == "__main__":
     
     if args.test_set:
         trainer.test(model)
-    # Save trained model
-    save_path = (args.save_path if args.save_path is not None else '/')
-    # save model weights
-    torch.save(trainer.model.resnet_model.state_dict(), save_path + 'trained_model.pt')
+    # Save trained model weights
+    torch.save(trainer.model.resnet_model.state_dict(), save_path + '/trained_model.pt')
